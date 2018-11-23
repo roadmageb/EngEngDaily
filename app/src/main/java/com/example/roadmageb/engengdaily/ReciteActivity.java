7b9424c2f1d1dd9a85c1f9791f9da16c0fc06f0c
류지석 <roadmageb@naver.com>
2018-11-24 03:42:46 +0900

    추가

--- app/src/main/java/com/example/roadmageb/engengdaily/ReciteActivity.java
+++ app/src/main/java/com/example/roadmageb/engengdaily/ReciteActivity.java
@@ -1,4 +1,12 @@
 package com.example.roadmageb.engengdaily;
 
-public class ReciteActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ReciteActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select);
+    }
 }
--- app/src/main/java/com/example/roadmageb/engengdaily/SelectActivity.java
+++ app/src/main/java/com/example/roadmageb/engengdaily/SelectActivity.java
@@ -3,7 +3,7 @@ package com.example.roadmageb.engengdaily;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ReciteActivity extends AppCompatActivity {
+public class SelectActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
