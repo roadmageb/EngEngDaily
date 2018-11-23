5aa99be8a1f72051bfed67d7bad2cd222e1ed3bc
류지석 <roadmageb@naver.com>
2018-11-24 03:42:36 +0900

    이름변경, 액티비티 추가

--- app/src/main/java/com/example/roadmageb/engengdaily/ReciteActivity.java
+++ app/src/main/java/com/example/roadmageb/engengdaily/ReciteActivity.java
@@ -1,13 +1,4 @@
 package com.example.roadmageb.engengdaily;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class ReciteActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recite);
-    }
+public class ReciteActivity {
 }
---
+++ app/src/main/java/com/example/roadmageb/engengdaily/SelectActivity.java
@@ -0,0 +1,13 @@
+package com.example.roadmageb.engengdaily;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReciteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select);
+    }
+}
